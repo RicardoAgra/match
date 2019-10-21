1ea22ff13857c9372b96d6936c3899585a2b7c0b
Ricardo Agra <ricardo@labtestanalyzer.com>
2019-10-21 20:09:07 +0100

    Versão 1.0.0

--- match.js
+++ match.js
@@ -14,7 +14,7 @@ const registerSW = (async () => {
 
 const SERVER_URL = "match-bilhar.herokuapp.com";
 const HTTP_URL = `https://${SERVER_URL}`;
-const WS_URL = `ws://${SERVER_URL}`;
+const WS_URL = `wss://${SERVER_URL}`;
 
 const Admin = new Vue({
   el: '#Match',
